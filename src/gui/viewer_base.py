2611160095091c443c4cba3c1b8e64c2bb54f382
unknown <EHWAAL@IC-LT-F2MPM4J.imtechict.local>
2014-02-13 22:34:38 +0100

    Added a rudimentary system for assigning work items to workers.

---
+++ src/gui/viewer_base.py
@@ -0,0 +1,241 @@
+'''
+Created on Feb 7, 2014
+
+@author: EHWAAL
+'''
+
+from PyQt4 import QtGui, QtCore
+from details_editor import DetailsViewer
+import model
+from util import bindLambda, mkMenu
+
+class ViewerBase(QtGui.QWidget):
+  def __init__(self, parent, decorator):
+    QtGui.QWidget.__init__(self, parent)
+    self.ui = decorator()
+    self.ui.setupUi(self)
+    
+    self.session = None
+
+  def getSession(self):
+    return self.session
+  
+  def clean(self):
+    raise NotImplementedError()
+  
+  def open(self, session):
+    self.session = session
+    self.clean()
+    
+  def openView(self, details):
+    ''' Called when an items needs to be viewed.
+    '''
+    pass
+      
+
+
+class ViewerWithDetailsBase(ViewerBase):
+  def __init__(self, *args):
+    ViewerBase.__init__(self, *args)
+    self.detail_items = {}    
+    self.details_viewer = None
+
+  def openDetailsViewer(self, details, read_only=False):
+    ''' Called when a different block is selected. '''
+    # Ensure there is no other viewer
+    self.closeDetailsViewer()
+    # Check there is something to view
+    if details is None:
+      return
+    # Create a new viewer.
+    widget = DetailsViewer(self.ui.areaDetails, details, self.session, read_only)
+    self.ui.areaDetails.setWidget(widget)
+    widget.show()
+    self.ui.areaDetails.show()
+    self.details_viewer = widget
+    widget.open_view.connect(self.openView)
+    
+  def closeDetailsViewer(self):
+    if self.details_viewer:
+      # Automatically commit any changes from the editor
+      with model.sessionScope(self.session):
+        widget = self.ui.areaDetails.takeWidget()
+        widget.close()
+        self.ui.areaDetails.hide()
+        self.details_viewer = None
+
+
+
+###############################################################################
+##
+def createTreeItem(details):
+  item = QtGui.QTreeWidgetItem()
+  item.setText(0, details.Name)
+  item.setFlags(QtCore.Qt.ItemFlags(QtCore.Qt.ItemIsEditable + 
+                    QtCore.Qt.ItemIsDragEnabled + 
+                    QtCore.Qt.ItemIsDropEnabled + 
+                    QtCore.Qt.ItemIsSelectable + 
+                    QtCore.Qt.ItemIsEnabled))
+  item.details = details
+  return item
+
+def makeModelItemTree(tree, model_class, parent):
+  ''' Turn a normal TreeWidget into a viewer for model items.
+  '''
+  # Create wrapper functions for event handlers
+  def addHandler(checked=False):
+    items = tree.selectedItems()
+    if len(items) == 1:
+      # Not a top-level item: try to find the ID of the item.
+      parent_item = items[0].details.Id
+    else:
+      parent_item = None
+      
+    details = model_class(Name='new item',
+                          Parent=parent_item)
+    with model.sessionScope(parent.getSession()) as session:
+      session.add(details)
+    # As the session is closed, the new item is added to this tree.
+    # Cause the item to be selected.
+    parent.onTreeItemAdded(details)
+    
+      
+  def deleteHandler(checked=False):
+    session = parent.getSession()
+    MB = QtGui.QMessageBox
+    items = tree.selectedItems()
+    if len(items) == 0:
+      return
+    reply = MB.question(parent, 'Weet u het zeker?',
+                               'De geselecteerde items verwijderen?',
+                               MB.Yes, MB.No)
+    if reply != MB.Yes:
+      return
+    
+    for item in items:
+      # Check the item has no children
+      if item.childCount() > 0:
+        MB.critical(parent, 'Sorry', "Het item heeft kinderen", MB.Ok)
+        return
+      parent_item = item.parent()
+      if parent_item:
+        index = parent_item.indexOfChild(item)
+        parent_item.takeChild(index)
+      else:
+        index = tree.indexOfTopLevelItem(item)
+        tree.takeTopLevelItem(index)
+      
+      if isinstance(item.details, model.ArchitectureBlock):
+        # check if there are connections or views of this block.
+        if session.query(model.BlockRepresentation).\
+                        filter(model.BlockRepresentation.Block==item.details.Id).count() != 0:
+          reply = MB.question(parent, 'Weet u het zeker?',
+                                     'Het blok wordt gebruikt in views. Toch verwijderen?',
+                                     MB.Yes, MB.No)
+          if reply != MB.Yes:
+            return
+        
+      session.delete(item.details)
+    try:
+      session.commit()
+    except:
+      session.rollback()
+      raise
+    
+  # Install the handlers as menu items
+  actions = [('Add', addHandler, {}), ('Delete', deleteHandler, {})]
+  mkMenu(actions, parent, tree)
+
+  # Cause items to be deselected when clicking in empty space.
+  def createHandler(widget):
+    def myPress(ev):
+      widget.clearSelection()
+      QtGui.QTreeWidget.mousePressEvent(widget, ev)
+  tree.mousePressEvent = createHandler(tree)
+  
+  def dropEvent(event):
+    ''' Replaces the dropEvent handler for the tree widgets.
+    '''
+    # Check that the drop is from within the widget.
+    if event.source() != tree:
+      event.ignore()
+      return
+    # Find out which item is dropped on.
+    item = tree.itemAt(event.pos())
+    # Check it is not dropped on itself
+    if item in tree.selectedItems():
+      event.ignore()
+      return
+    # Change the action from IGNORE to MOVE
+    event.setDropAction(QtCore.Qt.MoveAction)
+    # Get the current list of children
+    children = None
+    if item:
+      children = [item.child(i) for i in range(item.childCount())]
+    # Do the drop
+    result = QtGui.QTreeWidget.dropEvent(tree, event)
+    # Find out which item was dropped on, and administrate the changes.
+    with model.sessionScope(parent.getSession()):
+      if item:
+        new_children = [item.child(i) for i in range(item.childCount())]
+        new_children = [ch for ch in new_children if ch not in children]
+        parent_item = item.details.Id
+        for ch in new_children:
+          ch.details.Parent = parent_item
+      else:
+        # The dragged item has become a top-level item.
+        # Find out which item it was from the mime data.
+        details = parent.drop2Details(event)
+        details.Parent = None
+
+  # Override the dropEvent handler.
+  tree.dropEvent = dropEvent
+  
+  # Cause the parent to be informed when details are changed.
+  tree.itemChanged.connect(parent.onItemChanged)
+
+
+
+class ViewerWithTreeBase(ViewerWithDetailsBase):
+  def __init__(self, *args):
+    ViewerWithDetailsBase.__init__(self, *args)
+    self.tree_models = None
+    
+  def open(self, session):
+    ViewerWithDetailsBase.open(self, session)
+    # Populate the tree lists.    
+    for widget, cls in self.tree_models.iteritems():
+      root_items = self.session.query(cls).filter(cls.Parent==None).all()
+      self.populateTree(widget, root_items)
+
+  def populateTree(self, widget, root_items):
+    def addChildren(parent_item):
+      # Add all children
+      for c in parent_item.details.Children:
+        item = createTreeItem(c)
+        self.detail_items.setdefault(widget, {})[c.Id] = item
+        parent_item.addChild(item)
+        addChildren(item)
+    # Add the root items and their children
+    for r in root_items:
+      item = createTreeItem(r)
+      self.detail_items.setdefault(widget, {})[r.Id] = item
+      widget.addTopLevelItem(item)
+      addChildren(item)
+      
+  def onTreeItemClicked(self, item):
+    ''' Called when the user clicks on an item in a tree view.
+        Causes a details viewer to be opened for the tiems.
+    '''
+    self.openDetailsViewer(item.details)
+      
+  def onTreeItemAdded(self, details):
+    ''' Called when a new item was created in a tree.
+        Cause the focus to shift to the details editor for this item.
+    '''
+    # First cause the details editor to be shown.
+    self.onItemSelectionChanged(details)
+    # Set the focus to the 'Name' field for the editor
+    self.details_viewer.setFocusOnName()
+
+
---
+++ src/gui/workitem_view.py
@@ -0,0 +1,173 @@
+'''
+Created on Feb 7, 2014
+
+@author: EHWAAL
+'''
+
+from functools import partial
+from PyQt4 import QtCore, QtGui
+from design import WorkitemViewForm
+from details_editor import DetailsViewer
+from viewer_base import ViewerWithDetailsBase
+from gui.util import bindLambda
+import model
+
+
+
+# TODO: Default status must be Open: selecting Open must also show those without status.
+# TODO: Add filter showing current projects
+# TODO: Show 'Take this Item' when an item is selected. 
+
+def wrapListViewer(widget, parent):
+  pass
+
+
+
+
+class WorkitemView(ViewerWithDetailsBase):
+  def __init__(self, parent):
+    ViewerWithDetailsBase.__init__(self, parent, WorkitemViewForm[0])
+    
+    self.tables = [self.ui.tblCurrent, self.ui.tblItems]
+    self.table_contents = {w:[] for w in self.tables}
+    self.session = None
+
+    # fill the filter lists
+    layouts = [self.ui.grpType.layout(),
+               self.ui.grpPrio.layout(),
+               self.ui.grpStatus.layout()]
+    elements = [model.PlaneableItem.getItemTypeNames(),
+                model.PRIORITIES,
+                model.REQUIREMENTS_STATES]
+    for layout, texts in zip(layouts, elements):
+      for txt in texts:
+        btn = QtGui.QRadioButton(txt, parent=self)
+        layout.addWidget(btn)
+        btn.clicked.connect(self.onFilterChange)
+    # Also hook the existing 'all' buttons
+    self.ui.btnAllPrios.clicked.connect(self.onFilterChange)
+    self.ui.btnAllTypes.clicked.connect(self.onFilterChange)
+    self.ui.btnAllStates.clicked.connect(self.onFilterChange)
+    
+    # Hook the items table, for when the user selects a row.
+    for tbl in self.tables:
+      tbl.cellClicked.connect(partial(self.onCellClicked, widget=tbl))
+    
+    # Hook the Assign button
+    self.ui.btnAssign.clicked.connect(self.onAssign)
+            
+
+  def open(self, session):
+    self.session = session
+    ViewerWithDetailsBase.open(self, session)
+    # fill the combo boxes
+    users = session.query(model.Worker.Name).order_by(model.Worker.Name).all()
+    self.ui.cmbWorker.addItems([str(u[0]) for u in users])
+    # Show the items with the default filter
+    self.onFilterChange(None)
+    self.onWorkerChange()
+    
+  def clean(self):
+    self.ui.tblItems.clearContents()
+    self.table_contents = {w:[] for w in self.tables}
+    for wdg in [self.ui.cmbWorker, self.ui.cmbProject]:
+      wdg.clear()
+  
+  def onCellClicked(self, row, column, widget):
+    if row < len(self.table_contents[widget]):
+      item = self.table_contents[widget][row]
+      self.openDetailsViewer(item[0], read_only=True)
+
+  def onWorkerChange(self):
+    worker = str(self.ui.cmbWorker.currentText())
+    worker_id = self.session.query(model.Worker.Id).filter(model.Worker.Name==worker).one()[0]
+    items = self.getItems(status=model.OPEN_STATES,
+                          assigned_to=worker_id)
+    self.table_contents[self.ui.tblCurrent] = items
+    self.fillCurrentTable(items)
+    
+    
+  def fillCurrentTable(self, items):
+    ''' Fill the table with the items currently assigned to the worker.
+    '''
+    self.ui.tblCurrent.clearContents()
+    self.ui.tblCurrent.setRowCount(len(items))
+    for row, item in enumerate(items):
+      for col, txt in enumerate([item[0].Name, item[0].Priority, item[3]]):
+        if not txt is None:
+          self.ui.tblCurrent.setItem(row, col, QtGui.QTableWidgetItem(str(txt)))
+
+  def fillFiltererdTable(self, items):
+    ''' Show a list of items.
+        These items must have the following structure:
+          tuple(PlaneableItem, Status, AssignedTo '''
+    self.ui.tblItems.clearContents()
+
+    # Set the dimensions of the table
+    self.ui.tblItems.setRowCount(len(items))
+    # Fill the table
+    for row, item in enumerate(items):
+      for col, txt in enumerate([item[0].Name, item[0].Priority, item[1], item[2]]):
+        if not txt is None:
+          self.ui.tblItems.setItem(row, col, QtGui.QTableWidgetItem(txt))
+    
+
+  def onAssign(self, _checked):
+    ''' Called when the user wants to assign an item to a user. '''
+    worker = str(self.ui.cmbWorker.currentText())
+    index  = self.ui.tblItems.currentRow()
+    item   = self.table_contents[self.ui.tblItems][index]
+    worker_id = self.session.query(model.Worker.Id).filter(model.Worker.Name==worker).one()[0]
+    status = model.PlaneableStatus(Planeable=item[0].Id, AssignedTo=worker_id,
+                                   Status=item[1], TimeRemaining=item[3], TimeSpent=item[4])
+    with model.sessionScope(self.session):
+      self.session.add(status)
+    
+    # Update the views
+    self.onWorkerChange()
+    self.onFilterChange(False)
+
+  
+  def onFilterChange(self, _checked):
+    ''' Called when the filter selection has changed.
+        There are three parts to the filter: the type of item being shown,
+        the priority of the item, the status of the item'''
+    layouts = [self.ui.grpType.layout(),
+               self.ui.grpPrio.layout(),
+               self.ui.grpStatus.layout()]
+    selections = []
+    for layout in layouts:
+      for index in range(layout.count()):
+        btn = layout.itemAt(index).widget()
+        if not isinstance(btn, QtGui.QRadioButton):
+          continue
+        if btn.isChecked():
+          txt = str(btn.text())
+          if txt.lower() == 'all':
+            txt = None
+          selections.append(txt)
+          break
+    items = self.getItems(*selections)
+    self.table_contents[self.ui.tblItems] = items
+    self.fillFiltererdTable(items)
+    
+    
+  def getItems(self, item_type=None, priority=None, status=None, assigned_to=None):
+    ''' Query the database to get all work items meeting the filters in one go. '''
+    stmt = model.PlaneableStatus.getLatestQuery(self.session).subquery()
+    base = self.session.query(model.PlaneableItem, stmt.c.Status, stmt.c.AssignedTo,
+                              stmt.c.TimeRemaining, stmt.c.TimeSpent).\
+                             outerjoin(stmt).order_by(model.PlaneableItem.Name)
+    if not item_type is None:
+      base = base.filter(model.PlaneableItem.ItemType==item_type)
+    if not priority is None:
+      base = base.filter(model.PlaneableItem.Priority==priority)
+    if not status is None:
+      if isinstance(status, list):
+        base = base.filter(stmt.c.Status.in_(status))
+      else:
+        base = base.filter(stmt.c.Status == status)
+    base = base.filter(stmt.c.AssignedTo == assigned_to)
+      
+    return base.all()
+      
