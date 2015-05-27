c4a7aa58de0d95e9a513384ed848224331504cc6
Evert van de Waal <archtool@users.noreply.github.com>
2015-05-27 09:23:28 +0200

    Added a simple straight line between blocks

---
+++ src/svgeditor/test/unit/controllersSpec.js
@@ -0,0 +1,26 @@
+'use strict';
+
+/* jasmine specs for controllers go here */
+
+describe('PhoneCat controllers', function() {
+
+  describe('PhoneListCtrl', function(){
+    var scope, ctrl;
+
+    beforeEach(module('phonecatApp'));
+
+    beforeEach(inject(function($controller) {
+      scope = {};
+      ctrl = $controller('PhoneListCtrl', {$scope:scope});
+    }));
+
+    it('should create "phones" model with 4 phones', function() {
+      expect(scope.phones.length).toBe(4);
+    });
+
+
+    it('should set the default value of orderProp model', function() {
+      expect(scope.orderProp).toBe('age');
+    });
+  });
+});
